f7af7192c24f9394cd830498d14a01adc0341427
jamie9609 <714468438@qq.com>
2019-12-09 20:10:52 +0800

    study 2019.12.09

---
+++ Chapter9.3/mosaic.go
@@ -0,0 +1,38 @@
+package Chapter9_3
+
+import (
+	"image"
+	"image/color"
+)
+
+func averageColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+			r, g, b = r+float64(r1), g+float64(g1), b+float64(b1)
+		}
+	}
+	totalPixels := float64(bounds.Max.X * bounds.Max.Y)
+
+	var str [3] float64
+	str[0] = r / totalPixels
+	str[1] = g / totalPixels
+	str[2] = b / totalPixels
+	return str
+	//return [3]float64 {r / totalPixels, g / totalPixels, b / totalPixels}
+}
+
+func resize(in image.Image, newWidth int) image.NRGBA {
+	bounds := in.Bounds()
+	ratio := bounds.Dx() / newWidth
+	out := image.NewNRGBA(image.Rect(bounds.Min.X/ratio, bounds.Min.X/ratio, bounds.Max.X/ratio, bounds.Max.Y/ratio))
+	for y, j := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, j = y+ratio, j+1 {
+		for x, i := bounds.Min.X, bounds.Min.X; x < bounds.Max.X ; x, i = x+ratio, i+1  {
+			r, g, b, a := in.At(x, y).RGBA()
+			out.SetNRGBA(i, j , color.NRGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)})
+		}
+	}
+	return *out
+}
